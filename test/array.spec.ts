e3cc3ebe8be8e3df55f0e42b3a6cad57aefc3828
bryan-b <barryb.ebeh@gmail.com>
2023-10-07 23:03:11 +0100

    array_key_exists tested

--- test/array.spec.ts
+++ test/array.spec.ts
@@ -1124,3 +1124,53 @@ describe('array_is_list', () => {
         expect(array_is_list(arr)).toBe(false);
     });
 })
+
+describe('array_key_exists', () => {
+    it('should return true when key exists in array', () => {
+        const arr = { name: 'John', age: 25 };
+        expect(array_key_exists('name', arr)).toBe(true);
+    });
+
+    it('should return false when key does not exist in array', () => {
+        const arr = { name: 'John', age: 25 };
+        expect(array_key_exists('address', arr)).toBe(false);
+    });
+
+    it('should return false when key does not exist in empty array', () => {
+        const arr = {};
+        expect(array_key_exists('name', arr)).toBe(false);
+    });
+
+    it('should return true when key exists in array with null values', () => {
+        const arr = { name: null, age: null };
+        expect(array_key_exists('name', arr)).toBe(true);
+    });
+
+    it('should return true when numeric key exists in array', () => {
+        const arr = { 0: 'apple', 1: 'banana', 2: 'orange' };
+        expect(array_key_exists(1, arr)).toBe(true);
+    });
+
+    it('should return true when key exists in array with undefined values', () => {
+        const arr = { name: undefined, age: undefined };
+        expect(array_key_exists('name', arr)).toBe(true);
+    });
+});
+
+describe('array_key_first', () => {});
+describe('array_key_last', () => {});
+describe('array_keys', () => {});
+describe('array_map', () => {});
+describe('array_merge', () => {});
+describe('array_merge_recursive', () => {});
+describe('array_multisort', () => {});
+describe('array_pad', () => {});
+describe('array_product', () => {});
+describe('array_push', () => {});
+describe('array_rand', () => {});
+describe('array_reduce', () => {});
+describe('array_replace', () => {});
+describe('array_reverse', () => {});
+describe('array_search', () => {});
+describe('array_shift', () => {});
+describe('array_slice', () => {});
