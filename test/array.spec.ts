d4c7591b565cba6db6b7c119ca0c80f18a7de1b1
bryan-b <barryb.ebeh@gmail.com>
2023-10-08 00:20:48 +0100

    array_merge_recursive tested

--- test/array.spec.ts
+++ test/array.spec.ts
@@ -1240,6 +1240,7 @@ describe('array_keys', () => {
         expect(result).toEqual(['a', 'b', 'c']);
     });
 });
+
 describe('array_map', () => {
     it('should return an array of the same length as the input array when given a valid array and callback function', () => {
         const inputArray = [1, 2, 3];
@@ -1292,6 +1293,7 @@ describe('array_map', () => {
         expect(result).toEqual(expectedOutput);
     });
 });
+
 describe('array_merge', () => {
     it('should merge two arrays with different types of elements', () => {
         const arr1 = [1, 2, 3];
@@ -1329,7 +1331,55 @@ describe('array_merge', () => {
         expect(result).toEqual([1, null, 3, undefined, 5, 6]);
     });
 });
-describe('array_merge_recursive', () => {});
+
+describe('array_merge_recursive', () => {
+    it('should merge two arrays with different keys', () => {
+        const arr1 = { key1: 'value1' };
+        const arr2 = { key2: 'value2' };
+
+        const result = array_merge_recursive<Record<string, any>>(arr1, arr2);
+
+        expect(result).toEqual({ key1: 'value1', key2: 'value2' });
+    });
+
+    it('should merge two arrays with overlapping keys', () => {
+        const arr1 = { key1: 'value1', key2: 'value2' };
+        const arr2 = { key2: 'new value', key3: 'value3' };
+
+        const result = array_merge_recursive<Record<string, any>>(arr1, arr2);
+
+        expect(result).toEqual({ key1: 'value1', key2: 'new value', key3: 'value3' });
+    });
+
+    it('should merge three or more arrays with different keys', () => {
+        const arr1 = { key1: 'value1' };
+        const arr2 = { key2: 'value2' };
+        const arr3 = { key3: 'value3' };
+
+        const result = array_merge_recursive<Record<string, any>>(arr1, arr2, arr3);
+
+        expect(result).toEqual({ key1: 'value1', key2: 'value2', key3: 'value3' });
+    });
+
+    it('should merge empty arrays', () => {
+        const arr1 = {};
+        const arr2 = {};
+
+        const result = array_merge_recursive(arr1, arr2);
+
+        expect(result).toEqual({});
+    });
+
+    it('should merge arrays with null values', () => {
+        const arr1 = { key1: null };
+        const arr2 = { key2: null };
+
+        const result = array_merge_recursive<Record<string, any>>(arr1, arr2);
+
+        expect(result).toEqual({ key1: null, key2: null });
+    });
+});
+
 describe('array_multisort', () => {});
 describe('array_pad', () => {});
 describe('array_product', () => {});
