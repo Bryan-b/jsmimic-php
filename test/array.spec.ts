a5e00d30b11b48058fa6b8b1f81a03756336203b
bryan-b <barryb.ebeh@gmail.com>
2023-10-07 23:07:08 +0100

    Revert "array_key_first implemented and tested"

--- test/array.spec.ts
+++ test/array.spec.ts
@@ -1157,32 +1157,7 @@ describe('array_key_exists', () => {
     });
 });
 
-describe('array_key_first', () => {
-    it('should return the first key of a non-empty object', () => {
-        const obj = { key1: 'value1', key2: 'value2', key3: 'value3' };
-        expect(array_key_first(obj)).toBe('key1');
-    });
-
-    it('should return undefined for an empty object', () => {
-        const obj = {};
-        expect(array_key_first(obj)).toBeUndefined();
-    });
-
-    it('should return the first key of an object with numeric keys', () => {
-        const obj = { 1: 'value1', 2: 'value2', 3: 'value3' };
-        expect(array_key_first(obj)).toBe('1');
-    });
-
-    it('should return the first key of an object with numeric keys', () => {
-        const obj = { 1: 'value1', 2: 'value2', 3: 'value3' };
-        expect(array_key_first(obj)).toBe('1');
-    });
-
-    it('should return the first key of an object with non-string keys', () => {
-        const obj = { true: 'value1', false: 'value2', null: 'value3' };
-        expect(array_key_first(obj)).toBe('true');
-    });
-});
+describe('array_key_first', () => {});
 describe('array_key_last', () => {});
 describe('array_keys', () => {});
 describe('array_map', () => {});
