95846d32bb62ee9e8cc1d440fc7e5789fb7c4319
bryan-b <barryb.ebeh@gmail.com>
2023-10-08 04:32:25 +0100

    array_reduce tested

--- test/array.spec.ts
+++ test/array.spec.ts
@@ -1589,7 +1589,44 @@ describe('array_rand', () => {
     });
 });
 
-describe('array_reduce', () => {});
+describe('array_reduce', () => {
+    it('should sum up the numbers in the array when using a callback function', () => {
+        const arr = [1, 2, 3, 4, 5];
+        const callback = (accumulator: number, currentValue: number) => accumulator + currentValue;
+        const result = array_reduce(arr, callback);
+        expect(result).toBe(15);
+    });
+
+    it('should concatenate the strings in the array when using a callback function', () => {
+        const arr = ['Hello', ' ', 'World', '!'];
+        const callback = (accumulator: string, currentValue: string) => accumulator + currentValue;
+        const result = array_reduce(arr, callback);
+        expect(result).toBe('Hello World!');
+    });
+
+    it('should return the initial value when reducing an empty array with an initial value', () => {
+        const arr: number[] = [];
+        const callback = (accumulator: number, currentValue: number) => accumulator + currentValue;
+        const initial = 10;
+        const result = array_reduce(arr, callback, initial);
+        expect(result).toBe(10);
+    });
+    
+    it('should return the single element when reducing an array of one element without an initial value', () => {
+        const arr = [5];
+        const callback = (accumulator: number, currentValue: number) => accumulator + currentValue;
+        const result = array_reduce(arr, callback);
+        expect(result).toBe(5);
+    });
+
+    it('should return the sum of the element and the initial value when reducing an array of one element with an initial value', () => {
+        const arr = [5];
+        const callback = (accumulator: number, currentValue: number) => accumulator + currentValue;
+        const initial = 10;
+        const result = array_reduce(arr, callback, initial);
+        expect(result).toBe(15);
+    });
+});
 describe('array_replace', () => {});
 describe('array_reverse', () => {});
 describe('array_search', () => {});
